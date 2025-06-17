aafd5eccb933592c803a76184575eba0456bffbe
Aman <a.shalginbayev@yandex.com>
2025-06-17 07:57:53 +0500

    AAdded Common project

---
+++ Chapter6/Program.cs
@@ -0,0 +1,23 @@
+﻿using  static Common.Helper;
+
+int[] InsertionSort(int[] numbers) {
+	for (var i = 1; i < numbers.Length; i++) {
+		var temp_value = numbers[i];
+		var position = i - 1;
+
+		while (position >= 0) {
+			if (numbers[position] > temp_value) {
+				numbers[position + 1] = numbers[position];
+				position--;
+			}
+			else {
+				break;
+			}
+		}
+		
+		numbers[position + 1] = temp_value;
+	}
+	return numbers;
+}
+
+PrintElements(InsertionSort([1,-1,-123,0,5,2,126,8,12]));
---
+++ Common/Helper.cs
@@ -0,0 +1,11 @@
+﻿namespace Common;
+
+public static class Helper {
+    public static void PrintElements<T>(IEnumerable<T> elements)
+    {
+        foreach (var element in elements)
+        {
+            Console.Write($"{element} ");
+        }
+    } 
+}
