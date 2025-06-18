75dd52ecd825378936b9ac5e82e87afdf85d426a
Aman <a.shalginbayev@yandex.com>
2025-06-18 08:03:43 +0500

    Chapter6 ex. solutions

--- Chapter6/Program.cs
+++ Chapter6/Program.cs
@@ -20,4 +20,60 @@ int[] InsertionSort(int[] numbers) {
 	return numbers;
 }
 
-PrintElements(InsertionSort([1,-1,-123,0,5,2,126,8,12]));
+PrintElements(InsertionSort([1,-1,-123,0,5,2,126,8,12,-1288712]));
+
+// Ex6.1
+// 3N^2 + 2N + 1 = O(N^2)
+
+// Ex6.2
+// N + logN = O(N)
+
+// Ex6.3
+// In worst case scenario when numbers array does not have 2 number that adds up to 10, then the complexity is O(N^2) as its nested loop
+// In best case scenario when the first 2 numbers adds up to 10, the number of steps is 2, complexity is O(1)
+// In average case scenario number of steps will be somewhere between 2 and N^2, but its still O(N^2) as the number of total comparisons 
+// grows proportional to N² as the array grows.
+bool TwoSum(int[] numbers) {
+	for (var i = 0; i < numbers.Length; i++) {
+		for (var j = 0; j < numbers.Length; j++) {
+			if (i != j && numbers[i] + numbers[j] == 10) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+Console.WriteLine(TwoSum([1,2,3,1,5]));
+
+// O(N) complexity with extra O(N) memory allocation
+bool TwoSumLinear(int[] numbers) {
+	var seenNumbers = new HashSet<int>();
+	
+	foreach (var n in numbers) {
+		var target = 10 - n;
+		if (seenNumbers.Contains(target)) {
+			return true;
+		}
+		seenNumbers.Add(n);
+	}
+	return false;
+}
+
+Console.WriteLine($"TwoSumLinear([1,2,3,4,0]) => {TwoSumLinear([1,2,3,4,0])}");
+
+// Ex 6.4
+bool containsX(string word) {
+	var foundX = false;
+	foreach (var c in word) {
+		if (c == 'X') {
+			foundX = true;
+		}
+	}
+	return foundX;
+}
+
+
+
+
+
