3b0c9516dbd730501206ccd83365074095def0c9
Aman <a.shalginbayev@yandex.com>
2025-06-29 19:32:41 +0500

    Common edited

--- Common/Helper.cs
+++ Common/Helper.cs
@@ -3,9 +3,11 @@
 public static class Helper {
     public static void PrintElements<T>(IEnumerable<T> elements)
     {
+        Console.Write("\n [ ");
         foreach (var element in elements)
         {
             Console.Write($"{element} ");
         }
+        Console.Write(" ] ");
     } 
 }
