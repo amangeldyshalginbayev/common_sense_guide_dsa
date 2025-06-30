d71c643c310b271e29987d108174d9b45ee061b2
Aman <a.shalginbayev@yandex.com>
2025-06-30 17:54:47 +0500

    Chapter9 created

---
+++ Chapter9/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Chapter9");
+
+
+class Stack<T> {
+	private List<T> Data { get; set; }
+
+    public Stack()
+    {
+        Data = new List<T>();
+    }
+
+    public void Push(T element)
+    {
+        Data.Add(element);
+    }
+
+    public T Pop()
+    {
+        if (Data.Count == 0)
+            throw new InvalidOperationException("Stack is empty.");
+
+        T last = Data[^1];
+        Data.RemoveAt(Data.Count - 1);
+        return last;
+    }
+
+    public T Read()
+    {
+        return Data.LastOrDefault();
+    }
+}
