59f9485997ed72dc54f79dd9e73cd7a1634b9cba
Aman <a.shalginbayev@yandex.com>
2025-10-08 12:15:21 +0500

    Chapter13 finished

--- Chapter13/Program.cs
+++ Chapter13/Program.cs
@@ -1,9 +1,7 @@
 ﻿
 
 // Ex 13.1
-
-// 2 approaches, use built in sorting then choose top 3 O(nlogn) 
-// find 3 max values in one pass O(n)!
+// O(nlogn) 
 long FindGreatestProductBySorting(int[] numbers) {
 	if (numbers == null || numbers.Length < 3) {
 		throw new ArgumentOutOfRangeException("Min 3 numbers are required.");
@@ -19,6 +17,7 @@ long FindGreatestProductBySorting(int[] numbers) {
 	return product;
 }
 
+// O(n)
 int FindGreatestProduct(int[] numbers)
 {
     if (numbers == null || numbers.Length < 3)
@@ -26,7 +25,7 @@ int FindGreatestProduct(int[] numbers)
 
     int max1 = int.MinValue, max2 = int.MinValue, max3 = int.MinValue;
 
-    foreach (int num in numbers)
+    foreach (var num in numbers)
     {
         if (num > max1)
         {
@@ -47,4 +46,91 @@ int FindGreatestProduct(int[] numbers)
 
     return max1 * max2 * max3;
 }
+
+//Console.WriteLine(FindGreatestProduct([100,2,3,10,1,0,7,20,6,5,30]));
+
+// Ex 13.2
+// O(nlogn) + O(n) = O(nlogn)
+int FindMissingNumber(int[] numbers) {
+    if (numbers == null || numbers.Length == 0) {
+        throw new ArgumentOutOfRangeException();
+    }
+
+    // sort in nlog(n)
+    Array.Sort(numbers);
+
+    for (var i = 0; i < numbers.Length; i++) {
+        if (i != numbers[i]) {
+            return i;
+        }
+    }
+
+    return -1;    
+}
+
+//Console.WriteLine(FindMissingNumber([2,4,3,1,5]));
+//Console.WriteLine(FindMissingNumber([2,4,3,1,0]));
+//Console.WriteLine(FindMissingNumber([0]));
+//Console.WriteLine(FindMissingNumber([1]));
+
+// Ex 13.3
+
+int FindGreatestInQuadratic(int[] numbers)
+{
+    if (numbers == null || numbers.Length == 0)
+        throw new ArgumentOutOfRangeException();
+
+    int max = numbers[0];
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        bool isGreatest = true;
+
+        for (int j = 0; j < numbers.Length; j++)
+        {
+            if (numbers[i] < numbers[j])
+            {
+                isGreatest = false;
+                break;
+            }
+        }
+
+        if (isGreatest)
+            return numbers[i];
+    }
+
+    return max;
+}
+
+
+int FindGreatestInLog(int[] numbers) {
+
+    if (numbers == null || numbers.Length == 0) {
+        throw new ArgumentOutOfRangeException();
+    }
+
+    Array.Sort(numbers);
+
+    return numbers[numbers.Length-1];
+}
+
+int FindGreatestInLinear(int[] numbers) {
+    if (numbers == null || numbers.Length == 0) {
+        throw new ArgumentOutOfRangeException();
+    }
+
+    var max = numbers[0];
+
+    for (var i = 0; i < numbers.Length; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+    
+    return max;
+}
+
+Console.WriteLine(FindGreatestInQuadratic([1,3,-1,1298,7,6,3,4,6,15,8,8,0]));
+Console.WriteLine(FindGreatestInLog([1,3,-1,1298,7,6,3,4,6,15,8,8,0]));
+Console.WriteLine(FindGreatestInLinear([1,3,-1,1298,7,6,3,4,6,15,8,8,0]));
 
