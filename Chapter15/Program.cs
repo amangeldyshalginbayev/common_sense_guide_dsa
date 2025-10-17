121d097bfbb1454064d9d7a1a0a3d7803455fcf4
Aman <a.shalginbayev@yandex.com>
2025-10-17 17:59:44 +0500

    Chapter15 started

--- Chapter15/Program.cs
+++ Chapter15/Program.cs
@@ -1,1 +1,18 @@
 ﻿
+Console.WriteLine("");
+
+var node1 = new TreeNode(25);
+var node2 = new TreeNode(75);
+var root = new TreeNode(50, node1, node2);
+
+public class TreeNode {
+	public int Value { get; set; }
+	public TreeNode LeftChild { get; set; }
+	public TreeNode RightChild { get; set; }
+
+	public TreeNode(int value, TreeNode leftChild = null, TreeNode rightChild = null) {
+		Value = value;
+		LeftChild = leftChild;
+		RightChild = rightChild;
+	}
+}
