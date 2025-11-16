90256acba28289db6a84f7532fc30cd52662efbf
Aman <a.shalginbayev@yandex.com>
2025-11-16 22:24:07 +0500

    Ch16 practice

--- Chapter16/Program.cs
+++ Chapter16/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+class Heap
+{
+    List<int> Data = [];
+
+    int RootNode => Data.First();
+
+    int LastNode => Data.Last();
+
+    int LeftChildIndex(int index) => index * 2 + 1;
+
+    int RightChildIndex(int index) => index * 2 + 2;
+
+    int ParentIndex(int index) => (index - 1) / 2;
+
+    void Insert(int value)
+    {
+        // Insert as Last Node
+        Data.Add(value);
+
+        var newNodeIndex = Data.Count - 1;
+
+        // trickle up algorithm
+        while (newNodeIndex > 0)
+        {
+            int parentIndex = ParentIndex(newNodeIndex);
+
+            if (Data[newNodeIndex] <= Data[parentIndex])
+                break;
+
+            (Data[parentIndex], Data[newNodeIndex]) = (Data[newNodeIndex], Data[parentIndex]);
+
+            newNodeIndex = parentIndex;
+        }
+    }
+
+	void Delete() {
+		Data[0] = Data[Count - 1];
+		Data.RemoveAt(Data.Count - 1);
+
+		
+
+				
+
+	}
+}
