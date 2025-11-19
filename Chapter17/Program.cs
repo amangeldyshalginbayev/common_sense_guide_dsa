4b2dd03404b8415368815a05f30d429dec28a67b
Aman <a.shalginbayev@yandex.com>
2025-11-19 18:00:45 +0500

    Chapter17 started

---
+++ Chapter17/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+class TrieNode
+{
+	public Dictionary<char, TrieNode> Children { get; set; } = new();
+}
+
+class Trie
+{
+    TrieNode Root { get; set; } = new();
+
+	TrieNode Search(string word) {
+		var currentNode = Root;
+		foreach (var c in word) {
+			if (currentNode.Children.TryGetValue(c, out var val)) {
+				currentNode = val;
+			}
+			else {
+				return null;
+			}
+		}
+		
+		return currentNode;
+	}
+}
