91a50a2c4076e29a8faa5d02ca8c5fd8efabd371
Aman <a.shalginbayev@yandex.com>
2025-11-20 18:17:41 +0500

    changes

--- Chapter17/Program.cs
+++ Chapter17/Program.cs
@@ -4,24 +4,28 @@ Console.WriteLine("Hello, World!");
 
 class TrieNode
 {
-	public Dictionary<char, TrieNode> Children { get; set; } = new();
+    public Dictionary<char, TrieNode> Children { get; set; } = new();
 }
 
 class Trie
 {
     TrieNode Root { get; set; } = new();
 
-	TrieNode Search(string word) {
-		var currentNode = Root;
-		foreach (var c in word) {
-			if (currentNode.Children.TryGetValue(c, out var val)) {
-				currentNode = val;
-			}
-			else {
-				return null;
-			}
-		}
-		
-		return currentNode;
-	}
+    TrieNode Search(string word)
+    {
+        var currentNode = Root;
+        foreach (var c in word)
+        {
+            if (currentNode.Children.TryGetValue(c, out var val))
+            {
+                currentNode = val;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        return currentNode;
+    }
 }
